ed21a861bd468eccedce4f3ef4ea51154bfadff8
Ahmad Altalhoni <ahmadtalhoni455@gmail.com>
2023-03-22 22:27:09 +0300

    BusinessLayer

--- Core_Proje/Core_Proje/Controllers/HomeController.cs
+++ Core_Proje/Core_Proje/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using Core_Proje.Models;
+﻿using Core_proje.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -7,7 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Core_Proje.Controllers
+namespace Core_proje.Controllers
 {
     public class HomeController : Controller
     {
--- Core_Proje/Core_Proje/Models/ErrorViewModel.cs
+++ Core_Proje/Core_Proje/Models/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace Core_Proje.Models
+namespace Core_proje.Models
 {
     public class ErrorViewModel
     {
